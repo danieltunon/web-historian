dae2c2d94b88b3c01b3fd91ef36ac38c40f8af24
danieltunon <danieltunon@gmail.com>
2016-03-23 16:51:21 -0700

    Return index.html on GET request to '/'

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,46 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var url = require('url');
+var Promise = require('bluebird');
 // require more modules/folders here!
+var fs = require('fs');
+Promise.promisifyAll(fs);
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  console.log('request: ' + req.url);
+
+  var urlObj = url.parse(req.url, true);
+  // set headers
+  var headers = {
+    'content-type': 'text/html'
+  };
+    // content-type
+    // encoding
+
+  if (req.method === 'GET') {
+    if (urlObj.pathname === '/') {
+
+      
+      // read input.html file using a promise
+      fs.readFileAsync( path.join(__dirname, '/public/index.html'), 'utf8' )
+        .then(function(fileData) {
+           // set status code
+          res.writeHead(200, headers);
+          res.end(fileData);
+        })
+        .catch(function(err) {
+          res.writeHead(404, headers);
+          res.end('<!DOCTYPE html><html><body>Oops!!</body></html>');  
+        });
+      // do response.end sending it down
+
+    } else {
+      res.writeHead(404, headers);
+      res.end(archive.paths.list);
+
+    }
+  } else {
+    res.writeHead(404, headers);
+    res.end(archive.paths.list);
+  }
 };
